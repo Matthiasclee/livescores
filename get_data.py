b41a0c1b66dc5d5a1bb9c9eeda84d9b3abdf3c94
Matthias Lee <matthias@matthiasclee.com>
2024-12-04 19:02:01 -0800

    Fix error

--- get_data.py
+++ get_data.py
@@ -22,12 +22,12 @@ def get_data(team):
         team_json_data = urllib.request.urlopen(f"{TEAM_INFO_URL}{team}").read()
         image_json_data = urllib.request.urlopen(f"{IMAGE_INFO_URL}{team}").read()
 
-    if len(team_json_data) != 1:
-        return {"error": "Team Not Found"}
-
-    team_data = json.loads(team_json_data)["data"][0]
+    team_data = json.loads(team_json_data)["data"]
     image_data = json.loads(image_json_data)["data"]
 
-    data = { "team": team_data, "image": image_data, "error": False }
+    if len(team_data) != 1:
+        return {"error": "Team Not Found"}
+
+    data = { "team": team_data[0], "image": image_data, "error": False }
 
     return data
