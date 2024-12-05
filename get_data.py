553779a5e9a44e4f6a103256f5bf3715bde0dd07
Matthias Lee <matthias@matthiasclee.com>
2024-12-04 19:44:44 -0800

    Added error handling

--- get_data.py
+++ get_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 import urllib.request
 from settings import *
 
@@ -13,11 +14,20 @@ def get_data(team, data_source):
         team_json_data = urllib.request.urlopen(f"{TEAM_INFO_URL}{team}").read()
         image_json_data = urllib.request.urlopen(f"{IMAGE_INFO_URL}{team}").read()
     else:
-        team_file = open(f"score_archives/{data_source}/{team}_team.json")
+        if not os.path.exists(f"score_archives/{data_source}"):
+            return {"error": "Invalid data source"}
+
+        team_path = f"score_archives/{data_source}/{team}_team.json"
+        image_path = f"score_archives/{data_source}/{team}_image.json"
+
+        if not os.path.exists(team_path) or not os.path.exists(image_path):
+            return {"error": f"Team not found in data source {data_source}"}
+
+        team_file = open(team_path)
         team_json_data = team_file.read()
         team_file.close()
 
-        image_file = open(f"score_archives/{data_source}/{team}_image.json")
+        image_file = open(image_path)
         image_json_data = image_file.read()
         image_file.close()
 
@@ -25,7 +35,7 @@ def get_data(team, data_source):
     image_data = json.loads(image_json_data)["data"]
 
     if len(team_data) != 1:
-        return {"error": "Team Not Found"}
+        return {"error": "Team not found"}
 
     data = { "team": team_data[0], "image": image_data, "error": False }
 
