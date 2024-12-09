59b982dddea7dd264d801e8eaa23d8df3c544a89
Matthias Lee <matthias@matthiasclee.com>
2024-12-09 08:56:33 -0800

    Add inputs and outputs

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# This program is a discord bot that provides live scores from the CyberPatriot scoreboard.
+# Inputs: team ID and data source as provided in user's commands in discord
+# Outputs: team score and image data from the requested data source or live scoreboard
+
 # Import the bot code
 from bot import *
 
