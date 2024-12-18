48223fb65ac59c7ad1808fb29ef71e66e396c94d
Matthias Lee <matthias@matthiasclee.com>
2024-12-18 09:06:23 -0800

    Remove comments

--- settings.py
+++ settings.py
@@ -1,18 +1,10 @@
-# Import the json library
 import json
 
-# Get a setting
-# Arguments: - setting: setting name to get
-# Returns: setting value
 def get_setting(setting):
-    # Open the settings file
     settings_file = open("settings.json")
 
-    # Parse the file with json
     settings = json.load(settings_file)
 
-    # Close the file
     settings_file.close()
 
-    # Return the setting's value
     return settings[setting]
